1eb8b55b53eb18088acc89a3233ed563e11a0d2d
Grim-redd <mainasara.olulanke@stu.cu.edu.ng>
2023-04-30 00:57:33 +0100

    Minor oversight 🤭

--- pages/index.js
+++ pages/index.js
@@ -136,13 +136,13 @@ export default function Home() {
           </p>
           <div className="flex gap-6">
             <Link
-              className="px-8 py-2 text-black duration-300 bg-white rounded-lg hover:bg-black hover:text-white"
+              className="px-8 py-2 text-black duration-300 bg-white border rounded-lg hover:bg-black hover:text-white"
               href={"/cities"}
             >
               Deny
             </Link>
             <button
-              className="px-8 py-2 text-black duration-300 bg-white rounded-lg hover:bg-black hover:text-white"
+              className="px-8 py-2 text-black duration-300 bg-white border rounded-lg hover:bg-black hover:text-white"
               onClick={handleLocationClick}
             >
               Allow
